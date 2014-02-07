88bb02c1b34f57337002f81c1ca170d4dc074a1e
Hongsheng Yang <hs.yang@live.com>
2014-02-07 11:16:32 -0500

    add

---
+++ Matlab/3D_Vision/PLY_Utility/Write3DPointsAndFacesToPlyFile.m
@@ -0,0 +1,67 @@
+function ret = Write3DPointsToPlyFile(outFileName, pts, rgb, faces)
+% Write out a .ply file with 3D points and faces, optionally have color
+% texture
+
+    if nargin < 3
+        rgb = [];
+    end
+    
+    % arrange points in row order
+    % i.e. 
+    % x y z
+    % x y z
+    % ......
+    % x y z
+    if size(pts,1)==3 && size(pts,2)~=3
+        pts = pts';
+    end
+    
+    % points are arranged in row order
+    num = size(pts, 1);
+
+    fid = fopen(outFileName, 'w');
+    fprintf(fid, 'ply\r\n');
+    fprintf(fid, 'format ascii 1.0\r\n');
+    fprintf(fid, 'element vertex %d\r\n', num);
+    fprintf(fid, 'property float x\r\n');
+    fprintf(fid, 'property float y\r\n');
+    fprintf(fid, 'property float z\r\n');
+
+    data = pts;
+    % difference between uchar and diffuse_uchar?
+    if ~isempty(rgb)
+        fprintf(fid, 'property uchar red\r\n');
+        fprintf(fid, 'property uchar green\r\n');
+        fprintf(fid, 'property uchar blue\r\n');
+        data = [data single(rgb)];
+    end
+    
+    % output faces
+    numFaces = size(faces, 1);
+    fprintf(fid, 'element face %d\r\n', numFaces);
+    fprintf(fid, 'property list uint8 int32 vertex_indices\r\n');
+    
+
+    fprintf(fid, 'end_header\r\n');    
+
+
+    % for k = 1:num
+    %     fprintf(fid, '%f %f %f', pts(k, 1), pts(k, 2), pts(k, 3));
+    %     if ~isempty(color)
+    %         fprintf(fid, ' %u %u %u', color(k, 1), color(k, 2), color(k, 3));
+    %     end
+    %     fprintf(fid, '\r\n');
+    % end
+
+    if isempty(rgb)
+        fprintf(fid, '%f %f %f\r\n', data');
+    else
+        fprintf(fid, '%f %f %f %u %u %u\r\n', data');
+    end
+    
+    fprintf(fid, '%d %d %d %d %d\r\n', faces');
+    
+    fclose(fid);
+
+    ret = true;
+end
