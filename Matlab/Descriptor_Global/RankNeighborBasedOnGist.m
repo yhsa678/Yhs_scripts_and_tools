9779c83d4e787682d65dbb25ac9069e7d93ff1b1
Hongsheng Yang <hs.yang@live.com>
2014-02-14 11:36:48 -0500

    add selective list to rank subset of images

--- Matlab/Descriptor_Global/RankNeighborBasedOnGist.m
+++ Matlab/Descriptor_Global/RankNeighborBasedOnGist.m
@@ -1,11 +1,19 @@
-function imglist = RankNeighborBasedOnGist(imglist)
+function imglist = RankNeighborBasedOnGist(imglist, selRankList)
+    % rank images based on gist descriptor
+    % selRankList:
+    % image id that need to selectively ranked respective to others, avoid all
+    % pair-wise computation, by default: all pair-wise
 
     for k = 1:numel(imglist)
         fprintf(1, 'Calculating gist of no. %d of %d...\n', k, numel(imglist));
         imglist(k).gist = GetImageGist(imglist(k).img);
     end
     
-    for k1 = 1:numel(imglist)
+    if ~exist(selRankList, 'var')
+        selRankList = 1:numel(imglist);
+    end
+    
+    for k1 = selRankList
         fprintf(1, 'Ranking no. %d of %d...\n', k1, numel(imglist));
         dist = zeros(1, numel(imglist));
         gi = imglist(k1).gist;
