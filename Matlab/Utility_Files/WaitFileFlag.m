af648c9a3e95c8a2699cb2cffd2c1d63e8c6c1da
Hongsheng Yang <hs.yang@live.com>
2014-02-16 19:30:00 -0500

    correct

--- Matlab/Utility_Files/WaitFileFlag.m
+++ Matlab/Utility_Files/WaitFileFlag.m
@@ -1,8 +1,8 @@
 function flag = WaitFileFlag(fileflag, folder)
 % waiting [folder, fileflag] file, if the file is existed, then return,
 % otherwise waiting...
-if nargin < 2
-    folder = pwd;
+if ~exist('folder', 'var')
+    folder = '';
 end
 
 while (1)
