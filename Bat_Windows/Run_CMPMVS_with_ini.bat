f1f81ebfda13e37c8fa19ddc3d0b349cb7e45351
Hongsheng Yang <hs.yang@live.com>
2014-03-20 21:58:57 -0400

    update

--- Bat_Windows/Run_CMPMVS_with_ini.bat
+++ Bat_Windows/Run_CMPMVS_with_ini.bat
@@ -3,8 +3,12 @@ REM
 REM Run CMPMVS, using certain .ini file
 REM
 
-set cmpmvsDir=D:\Hongsheng\Test_Libraries_Runtime\SfM_MVS\CMPMVS_0_6_0\
-set cmpmvsDrive=D:
+REM set cmpmvsDir=D:\Hongsheng\Test_Libraries_Runtime\SfM_MVS\CMPMVS_0_6_0\
+REM set cmpmvsDrive=D:
+
+set cmpmvsDir=C:\Hongsheng\1_Test_Tools_Libraries\3_SfM_MVS\CMPMVS\CMPMVS_0_6_0
+set cmpmvsDrive=C:
+
 set taskIni=xxx
 set curDir=%~p0
 set curDrive=%~d0
